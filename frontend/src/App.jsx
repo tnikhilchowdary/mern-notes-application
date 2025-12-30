cc3ccda820ada9c7e9469ea6032c1f8baa6dcf7c
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-12-29 21:16:15 -0600

    Frontend Data Sended to Backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,39 @@ import "./index.css";
 function App() {
   const [notes, setNotes] = useState("");
   const [allNotes, setAllNotes] = useState([]);
+
   
+
+  const addNotes = async (e) => {
+    e.preventDefault();
+
+    try{
+      const res =  await axios.post("http://localhost:5000/notes",{
+        notes: notes
+      }
+      )
+      alert("added succesfully");
+    }
+    catch(error){
+      console.log("Error in adding notes")
+    }
+  }
   return (
     <div>
       <h1>Welcome to The Notes App</h1>
+      <form onSubmit={addNotes}>
+        <textarea
+        placeholder="Enter Notes"
+        value={notes}
+        onChange={(e) => setNotes(e.target.value)}
+        />
+        <div>
+        <button type="submit">Submit Notes</button>
+        </div>
+      </form>
+      <div>
+
+      </div>
     </div>
   );
 }
