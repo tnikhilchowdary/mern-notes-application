72aeb40aab870152ffa06e719a78de4a6d1fe952
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-12-29 20:33:32 -0600

    Notes Application frontend form setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [notes, setNotes] = useState([]);
+
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600 mb-4">
-        Tailwind is Working 🎉
-      </h1>
-
-      <p className="text-lg mb-4">Count: {count}</p>
-
-      <button
-        className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={() => setCount(count + 1)}
-      >
-        Increase
-      </button>
+    <div>
+      <h1>Welcome to the Notes App</h1>
+      <form>
+        <div>
+        <input type="text"
+        placeholder="Enter Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        </div>
+        <textarea 
+        placeholder="content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        />
+        <div>
+        <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
