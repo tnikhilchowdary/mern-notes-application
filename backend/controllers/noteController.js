57bc19d7216bfb2899bf0b4c771146ef503d93d1
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-12-29 18:33:19 -0600

    Add Data in Backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -16,3 +16,20 @@ export const getNotes = async (req, res) => {
     }
 }
 
+export const addNotes = async (req, res) => {
+    try{
+        const {notes} = req.body;
+        const addnotes = new note ({notes})
+        addnotes.save();
+        res.status(200).json({
+            message:"Notes added Succesfully",
+            addnotes
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Error in Adding Notes",
+            error:error.message
+        })
+    }
+}
--- backend/routers/noteRouter.js
+++ backend/routers/noteRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getNotes} from "../controllers/noteController.js";
+import {getNotes, addNotes} from "../controllers/noteController.js";
 
 const router = express.Router();
 
 router.get("/", getNotes);
+router.post("/", addNotes);
 
 export default router;
 
