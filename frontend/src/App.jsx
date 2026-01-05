a28861aca34bd5a97421bca9bd9c809d5c5d01cd
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-01-05 16:37:46 -0600

    Form added Succesfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,33 @@ import axios from "axios";
 import "./index.css";
 
 function App() {
-  
+  const [input, setInput] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try{
+      const response = await axios.post("http://localhost:5000/notes", {
+        notes:input
+      },
+      )
+      alert("Submitted Succcesfully")
+      window.location.reload();
+    }
+    catch(error){
+      console.log("Error in adding Notes");
+    }
+  }
   return (
     <div>
      <h1>Welome to the Notes App</h1>
+     <form onSubmit={handleSubmit}>
+      <input type="text"
+      placeholder="Enter Notes App"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+     </form>
     </div>
   );
 }
